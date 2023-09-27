a2ca3f7028db2b75fb985592a9eb80757720facb
nurhayatketenci <nurhayatktnn8@gmail.com>
2023-09-27 21:35:28 +0300

    last commit

--- src/main/java/com/example/flightsearch/dto/FlightInfo.java
+++ src/main/java/com/example/flightsearch/dto/FlightInfo.java
@@ -1,6 +1,8 @@
 package com.example.flightsearch.dto;
 
 import com.example.flightsearch.model.Airport;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Null;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,5 +15,6 @@ public class FlightInfoRequest {
     private Airport departureAirport;
     private Airport arrivalAirport;
     private LocalDateTime departureDatetime;
+    @Nullable
     private LocalDateTime returnDatetime;
 }
